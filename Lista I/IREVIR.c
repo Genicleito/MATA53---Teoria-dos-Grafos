33bd865d04178c7b867697430c9a12730bff4809
genicleito <genicleito@live.com>
2016-08-27 00:12:55 -0300

    Feito!

--- Lista I/IREVIR.c
+++ Lista I/IREVIR.c
@@ -1,71 +1,78 @@
+/*
+* Por: Genicleito Gonçalves
+* Disciplina: MATA53 - Teoria dos Grafos (2016.1)
+* Universidade Federal da Bahia (UFBA)
+*/
+
 #include <stdio.h>
 
-int main(){
-	int ini, fim, n, m, i, j, v, w, p;
-	
-	for(scanf("%d", &n), scanf("%d", &m); n != 0 && m != 0; scanf("%d", &n), scanf("%d", &m)){
-		int G[n][n], visitado[n], fila[n];
+int BFS(int n, int G[n][n], int *visitado, int *fila){
+	int i;
 
-		/* Inicialização da Matriz de Adjacência */
-	        for(i = 0; i < n; i++){
-	                for(j = 0; j < n; j++){
-	                        G[i][j] = 0;
-	                }
-	        }
+	for(i = 0; i < n; i++)
+		visitado[i] = 0;
+	visitado[0] = 1;
+
+	int ini = 0, fim = 0;
+	fila[fim++] = 0;
+
+	while(ini != fim){
+		int no = fila[ini++];
+		
+		for(i = 0; i < n; i++){
+			if(G[no][i] == 1){
+				if(!visitado[i]){
+					fila[fim++] = i;
+					visitado[i] = 1;
+				}
+			}
+		}
+	}
+
+	for(i = 0; i < n; i++){
+		if(!visitado[i]){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main(){
+	int n, m, i, j, v, w, p;
+	
+	for(scanf("%d %d", &n, &m); n != 0 && m != 0; scanf("%d %d", &n, &m)) {
+		int G[n][n], GR[n][n], visitado[n], fila[n];
+
+		/* Inicialização dos Grafos */
+	       for(i = 0; i < n; i++){
+	               for(j = 0; j < n; j++){
+	                       G[i][j] = 0;
+	                       GR[i][j] = 0;
+	               }
+	       }
 
 		for(i = 0; i < m; i++){
 			scanf("%d", &v);
 			scanf("%d", &w);
 			scanf("%d", &p);
 			G[v - 1][w - 1] = 1;
-			if(p == 2)
+			GR[w - 1][v - 1] = 1;	//	Grafo reverso de G
+			if(p == 2){
 				G[w - 1][v - 1] = 1;
+				GR[v - 1][w - 1] = 1;
+			}
 		}
 
 		/*	BFS	*/
-		for(i = 0; i < n; i++)
-			visitado[i] = 0;
-		visitado[v - 1] = 1;	//	v - 1 = Nó inicial (Vértice da última aresta inserida)
-
-		ini = fim = 0;
-		fila[fim++] = v - 1;
-
-		while(ini != fim){
-			int no = fila[ini++], grau = 0;
-			
-			for(i = 0; i < n; i++){
-				if(G[no][i] == 1){
-					grau++;
-					if(!visitado[i]){
-						fila[fim++] = i;
-						visitado[i] = 1;
-					}
-				}
-			}
-			if (grau == 0){
-				visitado[no] = 0;
-				ini = -1;
-				break;
-			}
-		}
-	
-		//printf("Teste %d\n", k);
-		//if(ini != fim)
-		if(ini == -1)
-			printf("0\n");
-//		else
-//			printf("1\n");
-		else{
-			for(i = 0; i < n; i++){
-				if(!visitado[i]){
-					printf("0\n");
-					break;
-				}
-			}
-			if(i == n)
+		if(BFS(n, G, visitado, fila)) {
+			if(BFS(n, GR, visitado, fila)) {
 				printf("1\n");
+			}else{
+				printf("0\n");
+			}
+		}else{
+			printf("0\n");
 		}
-
 	}
 	
 	return 0;
