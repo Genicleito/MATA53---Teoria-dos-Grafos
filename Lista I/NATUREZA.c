d9d81b50c89f36937e18743542d0a5a03924f96e
genicleito <genicleito@live.com>
2016-08-29 00:46:25 -0300

    Update

--- Lista I/NATUREZA.c
+++ Lista I/NATUREZA.c
@@ -8,18 +8,15 @@
 #include <string.h>
 
 typedef struct Grafo{
-	//int indice;
 	char nome[30];
 	int *adj;
 	int visitado;
 } Grafo;
 
-int BFS(int n, /*Grafo G[n]*/Grafo *G, int fila[n], int vertice){
+int BFS(int n, Grafo *G, int vertice){
 	int i, cont = 1;
+	int fila[n];
 
-/*	for(i = 0; i < n; i++)
-		visitado[i] = 0;
-	visitado[0] = 1;*/
 	G[vertice].visitado = 1;
 
 	int ini = 0, fim = 0;
@@ -38,62 +35,43 @@ int BFS(int n, /*Grafo G[n]*/Grafo *G, int fila[n], int vertice){
 			}
 		}
 	}
-/*
-	for(i = 0; i < n; i++){
-		if(!visitado[i]){
-			return 0;
-		}
-	}
-	*/
 	return cont;
 }
 
 int main(){
 	int i, j, k, c, r;
-	//Grafo G;
 	
 	for(scanf("%d %d", &c, &r); c != 0 && r != 0; scanf("%d %d", &c, &r)) {
-		int fila[c];
+		//int fila[c];
 		Grafo G[c];
 
-		/* Inicialização do Grafo */
+		/* Inicialização do Grafo e leitura dos nomes dos */
 	    for(i = 0; i < c; i++){
 			G[i].visitado = 0;
 			G[i].adj = (int *)calloc(c, sizeof(int));
-		}
-		//printf("G[i].adj[c] = %d\n", G[i].adj[c]);
-
-		for(i = 0; i < c; i++){
 			scanf(" %[^\n]s", G[i].nome);
-			//G[i].indice = i;		//	Talvez seja desnecessario
 		}
 
+		/* Leitura das relações e preencimento do grafo */
 		for (i = 0; i < r; i++){
 			char c1[30], c2[30];
 			scanf(" %s %s", c1, c2);
 
 			for(j = 0; strcmp(G[j].nome, c1) != 0; j++);
 			for(k = 0; strcmp(G[k].nome, c2) != 0; k++);
-// printf("Aqui1\n");
-// printf("j = %d\n", j);
-// printf("k = %d\n", k);
 
-// printf("G[j].adj[k] = %d\n", G[j].adj[k]);
-// printf("G[k].adj[j] = %d\n", G[k].adj[j]);
 			G[j].adj[k] = 1;
 			G[k].adj[j] = 1;
-// printf("Aqui2\n");
 		}
-/*		char linhaBranco[30];
-		scanf(" %[^\n]s", linhaBranco);
-*/
-		/*	BFS	*/
+
+		/* Realizar a busca em todos os vertices (que não foram visitados) e retornar
+		 * o número de vertices de cada componente conexa. */
 		int max = 0;
 		for(i = 0; i < c; i++){
 			if(G[i].visitado == 0){
-				int temp = BFS(c, G, fila, i);
+				int temp = BFS(c, G, i);	//	temp recebe a quantidade de vértices de uma certa componente conexa
 				if(temp > max)
-					max = temp;
+					max = temp;		// Se esse número de vertices for o maior, então essa é a maior componente conexa desse grafo
 			}
 		}
 		printf("%d\n", max);
