0ab52d966586ab8e2eb5052c69c14475b3583fba
genicleito <genicleito@live.com>
2016-09-08 23:40:48 -0300

    Update

--- Lista I/CriticalLinks.cpp
+++ Lista I/CriticalLinks.cpp
@@ -8,14 +8,13 @@
 using namespace std;
 
 int tempo;
-vector<int> criticalLinks;
+vector< pair<int, int> > criticalLinks;
 vector< vector<int> > grafo;
 int *low, *d, *pai, qtdCriticalLinks;
 
 void DFS(int vertice){
 	//visitado[vertice] = 1;
-	
-	//tempo++;
+
 	d[vertice] = tempo++;
 	low[vertice] = d[vertice];
 
@@ -24,29 +23,28 @@ void DFS(int vertice){
 		if(d[no] == -1){
 			pai[no] = vertice;
 			DFS(no);
-//printf("DebugPreIF\n");
-	//printf("low de %d = %d e d de %d = %d \n", grafo[vertice][i], low[ grafo[vertice][i] ], vertice, d[vertice]);
+
 			if(low[vertice] > low[no]){
 				low[vertice] = low[no];
-//printf("Debug\n");
+
 			}
 			if(low[no] == d[no]){
 				qtdCriticalLinks++;
-				criticalLinks.push_back(vertice);
-				criticalLinks.push_back(no);
+				if(vertice < no)
+					criticalLinks.push_back(make_pair(vertice, no));
+				else
+					criticalLinks.push_back(make_pair(no, vertice));
 			}
-			//low[vertice] = min(low[vertice], low[ grafo[vertice][i] ]);
 		}else if((pai[vertice] != no) && (low[vertice] > d[no])) {
 			low[vertice] = d[no];
 		}
 	}
-	//tempo++;
 }
-
+/*
 bool ordenarResultado (int x, int y) {
 	return (x < y);
 }
-
+*/
 int main(){
 	int n, x, v, y;
 
@@ -66,7 +64,7 @@ int main(){
 		
 		for(int j = 0; j < n; j++){
 			scanf("%d (%d)", &v, &x); // Lê os vértices e arestas
-//printf("%d %d <--\n", v, x);
+
 			for(int i = 0; i < x; i++){
 				scanf("%d", &y);
 				grafo[v].push_back(y);
@@ -83,16 +81,10 @@ int main(){
 		}
 
 		printf("%d critical links\n", qtdCriticalLinks);
-		sort(criticalLinks.begin(), criticalLinks.end(), ordenarResultado);
-		//qtdCriticalLinks = 0;
+		sort(criticalLinks.begin(), criticalLinks.end());//, ordenarResultado);
+		
 		for(int j = 0; j < (int) criticalLinks.size(); j++){
-			// printf("%d - %d\n", criticalLinks[k], criticalLinks[k + 1]);
-			// criticalLinks.pop_front();
-			// criticalLinks.pop_front();
-			printf("%d - ", criticalLinks[j]);
-			//criticalLinks.pop_front();
-			printf("%d\n", criticalLinks[++j]);
-			//criticalLinks.pop_front();
+			printf("%d - %d\n", criticalLinks[j].first, criticalLinks[j].second);
 		}
 		criticalLinks.clear();
 		printf("\n");
